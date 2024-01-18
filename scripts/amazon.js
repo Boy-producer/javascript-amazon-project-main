72a5cf2e397533f8e64a449ae8974d0e7526bf25
lance <lancelotnnadi@gmail.com>
2024-01-18 07:17:26 +0100

    used the product in the data

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,3 @@
-const products = [{
-   image: 'images/products/athletic-cotton-socks-6-pairs.jpg' ,
-   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-   rating: {
-    stars: 4.5,
-    counts: 87
-   },
-   priceCents: 1090
-},{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating:{
-        stars:4 ,
-        counts:127 
-    },
-    priceCents:  2095
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars:4.5,
-        counts: 56
-    },
-    priceCents: 799
-}
-    
-];
-
 
 let productHTML = ''
 
@@ -81,6 +53,6 @@ products.forEach((product) => {
 </div>`
 })
 
-console.log(productHTML)
+
 const gridContainer = document.querySelector('.js-product-grid');
 gridContainer.innerHTML = productHTML
